c6cf9a78b61eccb201aaed8be4afe187c1cec41d
rsk25 <montgomery7289@gmail.com>
2022-05-07 20:14:39 +0900

    book implementation

--- book_examples/chap3_Greedy/greedy3-3.py
+++ book_examples/chap3_Greedy/greedy3-3.py
@@ -3,13 +3,11 @@
 if __name__ == '__main__':
     # setup
     n, m = map(int, input().split())
-    min_num = 0
-    row_num = 0
-    while row_num < n:
+    result = 0
+
+    for i in range(n):
         row = map(int, input().split())
-        _min = min(row)
-        if min_num < _min:
-            min_num = _min
-        row_num += 1
-    
-    print(min_num)
+        min_num = min(row)
+        result = max(result, min_num)
+        
+    print(result)
