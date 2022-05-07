2c22f3c71738005238e70718ff72215f3a241602
rsk25 <montgomery7289@gmail.com>
2022-05-07 15:05:22 +0900

    first greedy problem

--- book_examples/chap3_Greedy/greedy3-1.py
+++ book_examples/chap3_Greedy/greedy3-1.py
@@ -1,14 +1,16 @@
+import sys
 
 def give_change(N: int) -> int:
-	assert N // 10 == 0
+	assert N % 10 == 0
 	
 	count = 0
 	coin_types = [500, 100, 50, 10]
 	
 	for coin in coin_types:
-		count += n // coin
-		n %= coin
+		count += N // coin
+		N %= coin
 	return count
 
 if __name__ == '__main__':
-	
+	change = give_change(int(sys.argv[1]))
+	print(change)
