5a6bc190c0741a8cf2f0d157110dfce515e52548
rsk25 <montgomery7289@gmail.com>
2022-05-10 12:28:38 +0900

    my implementation of 4-1

---
+++ book_examples/chap4_Implementation/implement4-1.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+if __name__ == '__main__':
+    n = int(input())
+    cmd = list(input().split())
+    start = [1, 1]
+
+    for c in cmd:
+        if c == 'R':
+            start[1] += 1
+            if start[1] > n:
+                start[1] -= 1
+        elif c == 'L':
+            start[1] -= 1
+            if start[1] <= 0:
+                start[1] += 1
+        elif c == 'U':
+            start[0] -= 1
+            if start[0] <= 0:
+                start[0] += 1
+        else:
+            start[0] += 1
+            if start[0] > n:
+                start[0] -= 1
+    
+    print(start[0], start[1])
