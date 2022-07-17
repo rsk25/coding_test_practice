e398c74717c6bff58678a2c264a590c73b18ab14
rsk25 <montgomery7289@gmail.com>
2022-07-17 10:59:25 +0900

    will pipeline multiple files to python script

--- book_examples/testing.sh
+++ book_examples/testing.sh
@@ -2,5 +2,15 @@
 
 DIR=$1
 PROG=$2
+CNT=$3
 
-cat ${DIR}/test/${PROG} | python ${DIR}/${PROG}.py
+
+if [[ $CNT -ge 2 ]]
+then
+    for ((i=1;i<$CNT+1;i++))
+    do
+        cat ${DIR}/test/${PROG}_${i} | python ${DIR}/${PROG}.py
+    done
+else
+    cat ${DIR}/test/${PROG} | python ${DIR}/${PROG}.py
+fi
